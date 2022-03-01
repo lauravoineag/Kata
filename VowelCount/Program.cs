be2e04489d117b02b1417c07312d4cf342ad47ad
lauravoineag <lauravoineag87@gmail.com>
2022-03-01 14:56:09 +0000

    Created While Loop Class

--- VowelCount/Program.cs
+++ VowelCount/Program.cs
@@ -1,10 +1,10 @@
-﻿/*int VowelCount(string text)
+﻿int VowelCount(string text)
 {
     char[] vowels={'a', 'e', 'i', 'o', 'u'};
     int total = 0;
 
     Console.WriteLine(vowels[0]);//test 
-    
+
     for (int i = 0; i < text.Length; i++)
     {
         for (int g = 0; g < vowels.Length; g++)
@@ -16,22 +16,21 @@
             }
         }
     }
-    
     return total;
 }
 
-var result=VowelCount(Console.ReadLine());
-Console.WriteLine(result);
-*/
+//var result=VowelCount(Console.ReadLine());
+//Console.WriteLine(result);
+
 
 int VowelCount2(string text)
 {
-    char[] vowels2={'a', 'e', 'i', 'o', 'u'};
+    char[] vowels={'a', 'e', 'i', 'o', 'u'};
     int total2 = 0;
 
     foreach (var character in text)
     {
-        foreach (var vowel in vowels2 )
+        foreach (var vowel in vowels )
         {
             if (character==vowel)
             {
@@ -42,9 +41,35 @@ int VowelCount2(string text)
     }
     return total2;
 }
+//var result2=VowelCount2(Console.ReadLine());
+//Console.WriteLine(result2);
 
-var result2=VowelCount2(Console.ReadLine());
-Console.WriteLine(result2);
+int VowelCount3(string text)
+{
+    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    int total3 = 0;
+
+    int i = 0;
+    while (i < text.Length)
+    {
+        int g = 0;
+        while (g < vowels.Length)
+        {
+            if (text[i] == vowels[g])
+            {
+                total3++;
+            }
+            g++;
+        }
+        i++;
+    }
+
+    return total3;
+}
+
+var result3=VowelCount3(Console.ReadLine());
+Console.WriteLine(result3);
+    
 
 
     
