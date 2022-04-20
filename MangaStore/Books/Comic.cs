94463615e17688d9cfcc67f3d266ba278274f3a3
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-04-20 11:42:11 +0100

    Set up constructor and added data for Comic.cs

--- MangaStore/Books/Comic.cs
+++ MangaStore/Books/Comic.cs
@@ -5,6 +5,13 @@ public class Comic
     public string Title { get; set; }
     public string Genre { get; set; }
     public string Description { get; set; }
-    public int Rating { get; set; }
+    public double Rating { get; set; }
 
+    public Comic(string title, string genre, string description, double rating)
+    {
+        Title = title;
+        Genre = genre;
+        Description = description;
+        Rating = rating;
+    }
 }
--- MangaStore/Program.cs
+++ MangaStore/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using MangaStore;
+
+Comic deadlyClass = new Comic ("Deadly Class", "Action -thriller","A student enrolled in an Atelier of the Deadly Arts, a secret boarding school in San Francisco, trains to become the next generation of assassins for crime families across the globe",7.6);
+Comic assassinsCreed = new Comic("Assassin's Creed", "Action-Adventure", "Nikolai Orelov, a member of the Russian Brotherhood of Assassins and his journey",7.0);
