d99936634593a06e1305b9d9aa2139e58d479595
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-04-22 12:55:40 +0100

    Added incomplete deserialisation for MangaStore

--- MangaStore/Program.cs
+++ MangaStore/Program.cs
@@ -1,4 +1,4 @@
-﻿using MangaStore;
+﻿string text = File.ReadAllText(@"C:\Code\Kata\MangaStore\Data.json");
+Console.WriteLine("Contents of WriteText.txt = {0}", text);
 
-Comic deadlyClass = new Comic ("Deadly Class", "Action -thriller","A student enrolled in an Atelier of the Deadly Arts, a secret boarding school in San Francisco, trains to become the next generation of assassins for crime families across the globe",7.6);
-Comic assassinsCreed = new Comic("Assassin's Creed", "Action-Adventure", "Nikolai Orelov, a member of the Russian Brotherhood of Assassins and his journey",7.0);
+Console.WriteLine("Hello Manga!");
