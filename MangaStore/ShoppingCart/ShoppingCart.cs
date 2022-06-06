5cb312b9834a63e06a7bf70476bc6742ba3962ad
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-06-06 15:11:44 +0100

    draft  Adding a product to ShoppingCart when a duplication occurs

--- MangaStore/ShoppingCart/ShoppingCart.cs
+++ MangaStore/ShoppingCart/ShoppingCart.cs
@@ -10,14 +10,33 @@ public class ShoppingCart
 
     public void Add(IProduct product)
     {
-        var cartItem = new CartItem(product); 
-        CartItems.Add(cartItem);
-        Total += cartItem.CalculateItem();
+        var cartItem = new CartItem(product);
+
+        foreach (var item in CartItems)
+        {
+            if (product.Name == cartItem.Product.Name)
+            {
+                cartItem.Quantity += 1;
+                Total += cartItem.CalculateItem();
+            }
+            else
+            {
+                CartItems.Add(cartItem);
+                Total += cartItem.CalculateItem();
+            }
+
+        }
     }
     
     public void Remove(IProduct product)
     {
-        CartItems.Remove(product);
-        Total -= product.Price;
+        var cartItem = new CartItem(product);
+        CartItems.Remove(cartItem);
+        Total -= cartItem.CalculateItem();
+    }
+
+    public double CalcutateTotal()
+    {
+        return Total;
     }
 }
