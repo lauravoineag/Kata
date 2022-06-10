03bafba116f0a6fd37f807ea90debd11045e69a2
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-06-10 13:42:34 +0100

    Created simple application for CLI read and write with user input

---
+++ CommandLine/Program.cs
@@ -0,0 +1,33 @@
+﻿while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "quit")
+    {
+        Console.WriteLine("Are you sure you want to quit?\n\n(Type YES to confirm or anything else to continue)");
+        string reply = Console.ReadLine();
+        if (reply == "YES")
+        {
+            break;  
+        }
+        continue;
+    }
+
+    var words = input.Split();
+    foreach (var word in words)
+    {
+        if (string.IsNullOrWhiteSpace(word))
+        { 
+            continue;//SKIP
+        }
+
+        bool isValidInt = int.TryParse(word, out int number);// bring back 2 values: bool, int-only used if bool true else defaults to zero
+        if (isValidInt)
+        {
+            Console.WriteLine(number + 10);//if true
+        }
+        else
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
