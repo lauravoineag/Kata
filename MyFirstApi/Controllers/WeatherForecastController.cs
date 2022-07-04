a470f8f01788ea4ba79c564387678642387dac34
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-07-04 15:20:04 +0100

    Alowed the user to specify a city for the weather forecast & added validation

--- MyFirstApi/Controllers/WeatherForecastController.cs
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -17,14 +17,17 @@ public class WeatherForecastController : ControllerBase
     };
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IActionResult Get(int days = 5)
+    public IActionResult Get(string city,int days = 5)
     {
         if (days > 14 || days < 1)
         {
             return BadRequest("Weather forecast only available for the next 14 days");
         }
 
-        var city = Cities[Random.Shared.Next(Cities.Length)];
+        if(!Cities.Contains(city))
+        {
+            return BadRequest("This city is not in our list of cities");
+        }
 
         var weatherForecasts = Enumerable.Range(0, days).Select(index => new WeatherForecast
             {
