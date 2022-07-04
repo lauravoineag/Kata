45e7c9557abc95a3761ca4f3595466052939efb8
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-07-04 14:28:43 +0100

    User can request no of days to forecast

--- MyFirstApi/Controllers/WeatherForecastController.cs
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -16,24 +16,19 @@ public class WeatherForecastController : ControllerBase
         "Vienna", "Copenhagen", "Zurich", "Geneva", "Frankfurt"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet(Name = "GetWeatherForecast")]
-    public CityForecast Get()
+    public CityForecast Get(int days)
     {
         var city = Cities[Random.Shared.Next(Cities.Length)];
-        var weatherForecasts= Enumerable.Range(0, 5).Select(index => new WeatherForecast
+        
+        var weatherForecasts= Enumerable.Range(0, days).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index).ToString("D"),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)],
         })
         .ToArray();
+        
         return new CityForecast()
         {
             City = city,
